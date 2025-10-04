63a2c3e0cdb7092adea49accc5e0424d7509778f
hosais <hosais@gmail.com>
2025-10-04 23:05:47 +0800

    add xml to replace POS template, Total Tax section

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "POS No Tax on Receipt",
+    "version": "15.0.1.0.0",
+    "category": "Point of Sale",
+    "summary": "Remove tax total line from POS receipt",
+    "author": "TZ hosais CJL",
+    "website": "https://github.com/hosais/no_tax_receipt",
+    "depends": ["point_of_sale"],
+    "data": [
+        "views/pos_receipt_no_tax_info.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
